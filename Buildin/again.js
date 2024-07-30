91bd13f08e87b24c0a50d3efc3993c1f293d69f3
Raman-2003 <ramankishore838@gmail.com>
2024-07-30 15:23:49 +0530

    remove value at end

--- Buildin/again.js
+++ Buildin/again.js
@@ -2,189 +2,111 @@ class Node{
     constructor(value){
         this.value = value;
         this.next = null;
+        this.prv = null;
     }
 }
 
-class Linked{
+class Double{
     constructor(){
         this.head = null;
-        this.size = 0
-    }
-
-    isEmpty(){
-        return this.size === 0
+        this.tail = null;
+        this.size = 0;
     }
 
     getSize(){
         return this.size;
     }
 
+    isEmpty(){
+        return this.size === 0;
+    }
+
     prepend(value){
-        const node = new Node(value)
+        let node = new Node(value);
         if(this.isEmpty()){
             this.head = node;
+            this.tail = node;
         }else{
             node.next = this.head;
+            this.head.prev = node;
             this.head = node;
         }
         this.size++
     }
 
     append(value){
-        const node = new Node(value)
+        let node = new Node(value);
         if(this.isEmpty()){
             this.head = node;
+            this.tail = node;
         }else{
-            let current = this.head;
-            while(current.next !== null){
-                current = current.next
-            }
-            current.next = node;
+            node.prev = this.tail;
+            this.tail.next = node;
+            this.tail = node;
         }
         this.size++
     }
 
-    insert(value, index){
-        if(index<0 || index>=this.size){
+    removeFrontValue(){
+        if(this.isEmpty()){
             return;
         }
-        if(index === 0){
-            this.prepend(value)
-        }else{
-            const node = new Node(value)
-            let current = this.head;
-            for(let i=0; i<index-1; i++){
-                current = current.next;
-            }
-            node.next = current.next;
-            current.next = node;
-            this.size++
-        }
-    }
-
-    removefrom(index){
-        if(index<0 || index>this.size){
-            return null;
-        }
-        let removeNode
-        if(index === 0){
-            removeNode = this.head;
-            this.head = removeNode.next
-            this.size--
-            return removeNode
-        }else{
-            let current = this.head;
-            for(let i=0; i<index-1; i++){
-                current = current.next
-            }
-            removeNode = current.next;
-            current.next = removeNode.next;
-            this.size--
-            return removeNode;
-        }
-    }
-
-    removeValue(value){
-        if(this.isEmpty()){
-            return 
-        }
         let removeValue;
-        if(value === this.head.value){
+        if(this.size === 1){
             removeValue = this.head;
-            this.head = removeValue.next
-            return removeValue;
+            this.head = null;
+            this.tail = null;
         }else{
-            let current = this.head;
-            while(current.next && current.next.value !== value){
-                current = current.next;
-            }
-            if(current.next){
-                removeValue = current.next;
-                current.next = removeValue.next;
-                return removeValue;
-            }
+            removeValue = this.head;
+            this.head.prev = null;
+            this.head = removeValue.next;
         }
         this.size--
+        return removeValue;
     }
 
-    removeDuplicates(){
+    removeValueEnd(){
         if(this.isEmpty()){
             return null;
         }
-        let curr = this.head;
-        let seenValues = new Set();
-        seenValues.add(curr.value)
-        
-        while(curr.next !== null){
-            if(seenValues.has(curr.next.value)){
-                    curr.next = curr.next.next;
-                    this.size--
-            }else{
-                seenValues.add(curr.next.value);
-                curr = curr.next;
-            }
+        if(this.size === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.tail = this.tail.prev;
+            this.tail.next = null;
         }
-    }
-
-    search(value){
-        let i=0;
-        let curr = this.head;
-        while(curr){ 
-              if(curr.value === value){
-                  return i;
-              }
-           curr = curr.next;
-           i++
-        }
-        return -1
-    }
-
-    reverse(){
-        let curr = this.head;
-        let prev = null;
-        while(curr){
-            let next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        this.head = prev;
+        this.size--
+       
     }
 
     print(){
         if(this.isEmpty()){
-            console.log('List is empty');
-        }else{
-            let current = this.head;
-            let listValues = ''
-            while(current !== null){
-                listValues += `${current.value} `
-                current = current.next;
-            }
-            console.log(listValues);
+            console.log('List is Empty');
         }
+        let listedValues = ''
+        let curr = this.head;
+        while(curr){
+            listedValues += `${curr.value} `
+            curr = curr.next;
+        }
+        console.log(listedValues);
     }
+    
 }
 
 
-const list = new Linked();
-list.prepend(30)
-list.prepend(20)
-list.prepend(10)
+const dou = new Double();
+dou.prepend(30)
+dou.prepend(20)
+dou.prepend(10)
+dou.append(40)
+dou.append(50)
+dou.print()
 
-list.append(40)
-list.append(50)
 
-list.insert(99, 2)
-// list.removefrom(2)
-// list.removeValue(99)
+dou.removeFrontValue()
+dou.print()
 
-list.print()
-
-console.log(list.search(99))
-
-list.reverse()
-list.print()
-
-// list.removeDuplicates()
-// list.print()
+dou.removeValueEnd()
+dou.print()
--- sample/one.js
+++ sample/one.js
@@ -1,70 +1,152 @@
-class Node{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
-}
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//     }
+// }
 
-class linked{
-    constructor(){
-        this.head = null;
-        this.size = 0
+// class linked{
+//     constructor(){
+//         this.head = null;
+//         this.size = 0
+//     }
+
+//     isEmpty(){
+//         return this.size === 0
+//     }
+
+//     getSize(){
+//         return this.size;
+//     }
+
+//     prepend(value){
+//         const node = new Node(value);
+//         if(this.isEmpty()){
+//             this.head = node;
+//         }else{
+//             node.next = this.head;
+//             this.head = node;
+//         }
+//         this.size++
+//     }
+
+//     append(value){
+//         const node = new Node(value);
+//         if(this.isEmpty()){
+//             this.head = node;
+//         }else{
+//             let curr = this.head;
+//             while(curr.next !== null){
+//                 curr = curr.next;
+//             }
+//             curr.next = node; 
+//         }
+//         this.size++
+//     }
+
+//     print(){
+//         if(this.isEmpty()){
+//             console.log('List is Empty');
+//         }else{
+//             let listedValues = ''
+//             let curr = this.head;
+//             while(curr){
+//                 listedValues += `${curr.value} `
+//                 curr = curr.next;
+//             }
+//             console.log(listedValues);
+//         }
+//     }
+// }
+
+
+// const lni = new linked()
+
+// lni.prepend(30)
+// lni.prepend(20)
+// lni.prepend(10)
+// lni.append(40)
+// lni.append(50)
+// lni.print()
+
+
+class CircularQueue{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.items = new Array(capacity)
+        this.front = -1;
+        this.rear = -1;
+        this.currentLength = 0;
+    }
+
+
+    isFull(){
+        return this.capacity === this.currentLength;
     }
 
     isEmpty(){
-        return this.size === 0
+        return this.currentLength === 0
     }
 
-    getSize(){
-        return this.size;
-    }
-
-    prepend(value){
-        const node = new Node(value);
-        if(this.isEmpty()){
-            this.head = node;
-        }else{
-            node.next = this.head;
-            this.head = node;
-        }
-        this.size++
-    }
-
-    append(value){
-        const node = new Node(value);
-        if(this.isEmpty()){
-            this.head = node;
-        }else{
-            let curr = this.head;
-            while(curr.next !== null){
-                curr = curr.next;
+    enqueue(value){
+        if(!this.isFull()){
+            this.rear = (this.rear+1)%this.capacity;
+            this.items[this.rear] = value;
+            this.currentLength += 1;
+            if(this.front === -1){
+                this.front = this.rear;
             }
-            curr.next = node; 
+        }else{
+            console.log('Queue is Full');
         }
-        this.size++
+    }
+
+    dequeue(){
+        if(this.isEmpty()){
+            return null;
+        }else{
+            let removeValue = this.items[this.front]
+            this.items[this.front] = null;
+            this.front = (this.front+1)%this.capacity;
+            this.currentLength -= 1;
+            if(this.isEmpty()){
+                this.rear = -1
+                this.front = -1;
+            }
+            return removeValue;
+        }
+       
+    }
+
+    peek(){
+        if(!this.isEmpty()){
+            return this.items[this.front];
+        }else{
+            return -1;
+        }
     }
 
     print(){
         if(this.isEmpty()){
-            console.log('List is Empty');
+            console.log('Queue is Empty');
         }else{
             let listedValues = ''
-            let curr = this.head;
-            while(curr){
-                listedValues += `${curr.value} `
-                curr = curr.next;
+            let i;
+            for(i=this.front; i !== this.rear; i=(i+1)%this.capacity){
+                listedValues += this.items[i] + " "
             }
+            listedValues += this.items[i]
             console.log(listedValues);
         }
     }
 }
 
 
-const lni = new linked()
+const cir = new CircularQueue(5)
+cir.enqueue(10)
+cir.enqueue(20)
+cir.enqueue(30)
+cir.dequeue()
+cir.print()
 
-lni.prepend(30)
-lni.prepend(20)
-lni.prepend(10)
-lni.append(40)
-lni.append(50)
-lni.print()
+console.log(cir.peek());
